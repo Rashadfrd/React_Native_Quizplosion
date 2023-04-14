8bcc3745181b9f3c5e2bfbe19a99124ee79f586a
Rashadfrd <rashadnf@code.edu.az>
2023-04-15 03:56:00 +0400

    landing page

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './components/Landing';
+
 
 export default function App() {
+
   return (
+    <>
+    <StatusBar style='light' />
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Landing />
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Landing/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+function Landing() {
+  return (
+    <View style={styles.landingSection}>
+      <Image style={styles.blobFirst} source={require('../../assets/quiz-5.png')} />
+      <Image style={styles.blobSecond} source={require('../../assets/quiz-6.png')} />
+      <View style={styles.appImageContainer}>
+        <Image
+          source={require("../../assets/quiz-2.webp")}
+          style={{ width: 370, height: 300, marginRight: 10 }}
+        />
+      </View>
+      <Text style={styles.appTitle}>
+        <Image 
+          source={require("../../assets/quiz-4.png")}
+          style={{width:50,height:50}}  
+        />
+        uizPlosion
+      </Text>
+      <Text style={styles.appDesc}>
+        Biliyinə güvənirsən? O zaman Quizplosion ilə özünü test et!
+      </Text>
+      <TouchableOpacity activeOpacity={0.7} style={styles.startBtn}>
+        <Text style={{ fontSize: 24, fontWeight: "500" }}> Başla</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  landingSection: {
+    flex: 1,
+    backgroundColor: "#1c1a5e",
+    paddingVertical: 35,
+    paddingHorizontal: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+    position: "relative",
+  },
+  appTitle: {
+    fontSize: 45,
+    fontWeight: "500",
+    textAlign: "center",
+    color: "#fff",
+  },
+  appDesc: {
+    textAlign: "center",
+    width: "95%",
+    color: "#fff",
+    fontSize: 18,
+  },
+  startBtn: {
+    marginTop: 30,
+    backgroundColor: "#41e5ed",
+    width: "80%",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  blobFirst:{
+    position:'absolute',
+    left:0,
+    top:-20
+  },
+  blobSecond:{
+    position:'absolute',
+    bottom:-135,
+    right:30
+  }
+});
+
+export default Landing;
